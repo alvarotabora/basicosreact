ab3dcb51d341f40211ef32d2173d1859dd96d31b
alvarotabora <41582433+alvarotabora@users.noreply.github.com>
2019-08-06 18:50:58 -0600

    Carga inicial

---
+++ src/components/ListaProductos.js
@@ -0,0 +1,57 @@
+import React, { Component, Fragment } from 'react';
+import Producto from './Producto';
+
+class ListaProductos extends Component
+{
+    state = {
+        productos: []
+    }
+
+    componentDidMount()
+    {
+        console.log(1);
+
+        this.setState({
+            productos: [
+                { id: 1, nombre: 'Camisa ReactJS', precio: 30 },
+                { id: 2, nombre: 'Camisa VuetJS', precio: 30 },
+                { id: 3, nombre: 'Camisa Angular', precio: 30 },
+                { id: 4, nombre: 'Camisa Node.js', precio: 30 }
+            ]
+        })
+    }
+    componentWillMount()
+    {        
+        console.log(2);
+    }
+    componentWillUpdate()
+    {   
+        console.log(3);
+    }
+    componentWillUnmount()
+    {    
+        console.log(5);
+    }
+
+
+    render()    
+    {
+        console.log(5);
+
+        const { productos } = this.state;        
+
+        return (
+            <Fragment>
+                <h1>Lista de Productos</h1>
+                {
+                    productos.map(producto => (
+                        <Producto
+                            key={producto.id}
+                            producto={producto} />
+                    ))}
+            </Fragment>
+        );
+    }
+}
+ 
+export default ListaProductos;
